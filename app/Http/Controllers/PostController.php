42d7ddc39efa211513bd4dd2235dc060e34fb786
Kirabou <kirabou1942@gmail.com$ git config --global user.email kirabou1942@gmail.com$ git config --global user.email>
2024-12-26 20:49:21 +0900

    適切なコミットメッセージ

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -1,20 +1,14 @@
 <?php
 
 namespace App\Http\Controllers;
-
 use App\Models\Post;
+
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
     public function index(Post $post)
     {
-        return view('posts.index')->with(['posts' => $post->getByLimit()]);
+        return view('posts.index')->with(['posts' => $post->getPaginateByLimit(1)]);
     }
-    
-    public function show(Post $post)
-    {
-        dd($post);
-        return view('posts.show')->with(['post' => $post]);
-    }  
 }    
